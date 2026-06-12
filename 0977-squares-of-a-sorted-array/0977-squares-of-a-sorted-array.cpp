e66b0df1b6f24880fa0d9c17001a364e9091130d
Rawat-Nikhil1 <nikhil.rawat7440@gmail.com>
2026-06-12 08:31:36 -0300

    Time: 4 ms (39.14%), Space: 31.9 MB (11.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,13 +2,56 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
+        int res = 0;
         vector<int> ans(n);
+        vector<int> negativeArray;
+        vector<int> positiveArray;
 
         for (int i = 0; i < n; i++) {
-            ans[i] = nums[i] * nums[i];
+            if (nums[i] < 0) {
+                negativeArray.push_back(nums[i]);
+            } else {
+                positiveArray.push_back(nums[i]);
+            }
         }
 
-        sort(ans.begin(), ans.end());
+        for (int i = 0; i < negativeArray.size(); i++) {
+            negativeArray[i] = negativeArray[i] * negativeArray[i];
+        }
+
+        for (int i = 0; i < positiveArray.size(); i++) {
+            positiveArray[i] = positiveArray[i] * positiveArray[i];
+        }
+
+        reverse(negativeArray.begin(), negativeArray.end());
+
+        int i = 0;
+        int j = 0;
+
+        while (i < negativeArray.size() && j < positiveArray.size()) {
+            if (negativeArray[i] > positiveArray[j]) {
+                ans[res] = positiveArray[j];
+                j++;
+                res++;
+            } else {
+                ans[res] = negativeArray[i];
+                i++;
+                res++;
+            }
+        }
+
+        while (i < negativeArray.size()) {
+            ans[res] = negativeArray[i];
+            i++;
+            res++;
+        }
+
+        while (j < positiveArray.size()) {
+            ans[res] = positiveArray[j];
+            j++;
+            res++;
+        }
         return ans;
     }
+    
 };
