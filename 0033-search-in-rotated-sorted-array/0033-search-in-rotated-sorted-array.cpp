9b07655d199c37b3d4f4a8fa563d281156fec446
Rawat-Nikhil1 <nikhil.rawat7440@gmail.com>
2026-07-14 12:18:48 +0530

    Time: 0 ms (100%), Space: 15.1 MB (96.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,23 +8,35 @@ public:
         int mid = s + (e - s) / 2;
 
         while (s <= e) {
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            else if (nums[s] <= nums[mid]) {
-                if (nums[s] <= target && target < nums[mid]) {
-                    e = mid - 1;
-                } else {
+            if (nums[mid] > nums[n - 1]) { // PART 1
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                else if (nums[mid] < target) {
                     s = mid + 1;
                 }
-            }
-
-            else {
-                if (nums[mid] < target && target <= nums[e]) {
-                    s = mid + 1;
-                } else {
-                    e = mid - 1;
+                else {
+                    if (nums[0] > target) {
+                        s = mid + 1;
+                    } else {
+                        e = mid - 1;
+                    }
+                }
+            } 
+            
+            else { // PART 2
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                else if (nums[mid] > target) {
+                    e = mid-1;;
+                }
+                else {
+                    if (nums[n-1] < target) {
+                        e = mid-1;
+                    } else {
+                        s = mid+1;
+                    }
                 }
             }
             mid = s + (e - s) / 2;
