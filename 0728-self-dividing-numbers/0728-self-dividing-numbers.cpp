ce6719564ea8a9af948e126a207ef864fdf126c1
Rawat-Nikhil1 <nikhil.rawat7440@gmail.com>
2026-08-20 23:14:14 +0530

    Time: 0 ms (100%), Space: 9.3 MB (27.41%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool check(int n) {
+        int temp = n;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+
+            if (digit == 0 || n % digit != 0) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+
+        for (int i = left; i <= right; i++) {
+            if (check(i)) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
