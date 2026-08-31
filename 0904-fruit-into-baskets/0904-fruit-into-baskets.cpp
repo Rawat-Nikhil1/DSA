20fbc384b9496217ea24a9f74d1dbca05afc837c
Rawat-Nikhil1 <nikhil.rawat7440@gmail.com>
2026-08-31 12:46:43 +0530

    Time: 51 ms (27.84%), Space: 84.8 MB (63.08%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        map<int,int>freq;
-        int low = 0;
-        int res = -1;
+        unordered_map<int, int> f;
         int n = fruits.size();
+        int low = 0;
+        int res = 0;
 
-        for( int high=0 ; high<n ; high++){
-            freq[fruits[high]]++;
+        for (int high = 0; high < n; high++) {
+            f[fruits[high]]++;
 
-            while(freq.size() > 2){
-                freq[fruits[low]]--;
-                 
-               if(freq[fruits[low]]==0){
-                   freq.erase(fruits[low]);
-               }
-               low++; 
+            while (f.size() > 2) {
+                f[fruits[low]]--;
+                if (f[fruits[low]] == 0) {
+                    f.erase(fruits[low]);
+                }
+                low++;
             }
-            if(freq.size() < 2 || freq.size()==2){
-                int len = high-low+1;
-                res = max(len,res);
+
+            if (f.size() < 2 || f.size() == 2) {
+                int len = high - low + 1;
+                res = max(len, res);
             }
         }
         return res;
