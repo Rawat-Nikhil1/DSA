82cbc669ec9e28f6027d287d412504e9a6d38f1a
Rawat-Nikhil1 <nikhil.rawat7440@gmail.com>
2026-09-02 21:39:14 +0530

    Time: 1 ms (26.39%), Space: 60.9 MB (26.21%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -34,7 +34,27 @@ public:
 
     void recoverTree(TreeNode* root) {
         fun(root);
-        sort(res.begin(), res.end());
+        int n = res.size();
+        int mistake = 0;
+        int m1first, m1second, m2first, m2second;
+        for (int i = 0; i < n - 1; i++) {
+            if (res[i] > res[i + 1]) {
+                if (mistake == 0) {
+                    m1first = i;
+                    m1second = i + 1;
+                    mistake++;
+                } else {
+                    m2first = i;
+                    m2second = i + 1;
+                    mistake++;
+                }
+            }
+        }
+        if (mistake == 1) {
+            swap(res[m1first], res[m1second]);
+        } else {
+            swap(res[m1first], res[m2second]);
+        }
         fix(root);
     }
 };
